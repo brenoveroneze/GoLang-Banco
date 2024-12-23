4b6e7fb80e322afb594b25484f0b6a035502bde8
Breno Veroneze <oliveirabreno.veroneze@gmail.com>
2024-12-22 21:53:02 -0300

    Curso finalizado

---
+++ contas/contaCorrente.go
@@ -0,0 +1,46 @@
+package contas
+
+import cliente "banco/clientes"
+
+type ContaCorrente struct {
+	Titular       cliente.Titular
+	NumeroAgencia int
+	NumeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	podeDepositar := valorDoDeposito > 0
+	if podeDepositar {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito negativo", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
+}
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"banco/contas"
+	"fmt"
+)
+
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
+
+	//clienteBreno := clientes.Titular{Nome: "Breno", CPF: "123.123.123-12", Profissao: "Analista"}
+	contaDoBreno := contas.ContaPoupanca{}
+
+	contaDoBreno.Depositar(100)
+	PagarBoleto(&contaDoBreno, 60)
+	fmt.Println(contaDoBreno.ObterSaldo())
 
 }
